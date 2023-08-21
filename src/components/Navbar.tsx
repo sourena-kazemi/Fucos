3d0a6b2a069631ab205e0b39d0f1d5a3ad7e9036
Your Namesourena-kazemi$ git config --global user.name Your <kazemisourena@gmail.com>
2023-08-21 15:27:07 +0330

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,64 @@
+import { useState } from "react"
 type props = {
   isUserLoggedIn: boolean
 }
 function Navbar({ isUserLoggedIn }: props) {
+  const [isSideBarOpen, setIsSideBarOpen] = useState<boolean>(false)
   return (
-    <nav>
-      <div>
-        <img src="/Fucos.svg" alt="Fucos Icon" />
+    <nav className="mt-5 flex justify-between mx-5 items-center md:mt-10 md:mx-10">
+      <div className="flex items-center gap-2 z-10">
+        <img src="/Fucos.svg" alt="Fucos Icon" className="w-8" />
+        <p className="text-green text-2xl md:text-3xl">Fucos</p>
       </div>
-      <div></div>
+      <div
+        className="flex flex-col gap-y-2 group z-10 md:hidden"
+        onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+      >
+        <div className="rounded w-11 h-1 bg-lightGreen group-hover:w-14 transition-all"></div>
+        <div className="rounded w-14 h-1 bg-lightGreen transition-all"></div>
+        <div className="rounded w-11 h-1 bg-lightGreen self-end group-hover:w-14 transition-all"></div>
+      </div>
+      <div
+        className={`fixed bg-darkGray w-full h-full top-0 transition-all flex flex-col items-center justify-center gap-y-10 text-2xl md:hidden ${
+          isSideBarOpen ? "left-0" : "left-full"
+        }`}
+      >
+        <a href="#">Log In</a>
+        <a href="#">Sign Up</a>
+        <a href="#" className={`${isUserLoggedIn ? "" : "hidden"}`}>
+          Log Out
+        </a>
+        <a href="#">
+          <img
+            src="/github.svg"
+            alt="github logo"
+            className="w-10 hover:scale-105 transition-all"
+          />
+        </a>
+      </div>
+      <div className="gap-x-10 items-center hidden text-xl md:flex">
+        <a href="#" className="hover:text-green transition-colors">
+          Log In
+        </a>
+        <a href="#" className="hover:text-green transition-colors">
+          Sign Up
+        </a>
+        <a
+          href="#"
+          className={`hover:text-green transition-colors ${
+            isUserLoggedIn ? "" : "hidden"
+          }`}
+        >
+          Log Out
+        </a>
+        <a href="#">
+          <img
+            src="/github.svg"
+            alt="github logo"
+            className="w-10 hover:scale-105 transition-all"
+          />
+        </a>
+      </div>
     </nav>
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,4 @@
-import { Link } from "react-router-dom"
 function LandingPage() {
-  return (
-    <Link className="text-3xl text-emerald-600" to="/test">
-      Hello From Landing Page
-    </Link>
-  )
+  return <p>home page</p>
 }
 export default LandingPage
